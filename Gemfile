fbdd6586068ff84492e24242dc02c3611c5efc31
Laura Díaz <valentina.hp@gmail.com>
2014-01-19 14:33:57 -0430

    Eliminando Devise

--- Gemfile
+++ Gemfile
@@ -34,8 +34,6 @@ group :doc do
   gem 'sdoc', require: false
 end
 
-gem 'devise'
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,20 +1,10 @@
 class Usuario < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable
   self.table_name = "Usuario"
   after_initialize :relleno_usuario
   validates :nombre_usuario, presence: true
-  def email_required?
-	  false
-	end
 
-	def email_changed?
-	  false
-	end
   def relleno_usuario
   	self.estatus = 'A'
-  	Usuario.count
   	if self.codigo_usuario.nil?
   		self.codigo_usuario = Usuario.count + 1
   	end
