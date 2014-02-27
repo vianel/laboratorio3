2db5313fd988bd0f0ca2891d91f7b7cb8e42e062
vianel <vnl1990@gmail.com>
2014-02-27 12:30:58 -0430

    Acomodado leve bug en el login

--- app/controllers/arboles_controller.rb
+++ app/controllers/arboles_controller.rb
@@ -17,7 +17,7 @@ class ArbolesController < ApplicationController
 		end
 		resultado << "]"
 =end
-	    	Arbol.where(rol_id: $usuario.rol_id, status: 'A').each do |arbol|
+	    Arbol.where(rol_id: $usuario.rol_id).each do |arbol|
 
 			resultado << {rol_id: arbol.rol_id, text: arbol.texto, vinculo: arbol.vinculo,leaf: true}
 		
